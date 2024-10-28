7f1290f0af3a3fd78aff21f13376be2a2684c114
Auvinen Jenny Kaarina <jenny.k.auvinen@student.jyu.fi>
2024-10-28 15:32:18 +0200

    Pelikenttä

--- Siili/Siili/Siili.cs
+++ Siili/Siili/Siili.cs
@@ -10,18 +10,24 @@ namespace Siili;
 /// @author jenny
 /// @version 28.10.2024
 /// <summary>
-/// 
+/// Siili Hippu lähtee tutkimaan kotimetsäänsä kerätäkseen talvivarastoon marjoja ja hedelmiä ennen talven tuloa. Matkallaan hän kohtaa esteitä, 
+/// kuten kiviä ja oksia, joita täytyy ylittää hyppäämällä.
+//  Pelaaja ohjaa Siili Hippua keräämään ruokaa talvivarastoon ennen talven saapumista. Tavoitteena on kerätä riittävä määrä marjoja ja hedelmiä.
 /// </summary>
+
+/// TODO: - Suunnitellaan pelin rakenne -> luodaan pelin taso -> lisätään esteet ja keräilyesineet.
+//  Luodaan pelihahmo ja määritellään perusliikkeet (kävely, hyppy).
 public class Siili : PhysicsGame
 {
     public override void Begin()
     {
-        //Siili peliin
-        Level.Background.Color = Color.Green;
-        PhysicsObject olio = new PhysicsObject(100, 50);
-        olio.Shape = Shape.Circle;
-        olio.Color = Color.Brown;
-        Add(olio);
+        //Siili peliin 
+        Level.Background.CreateGradient(Color.Green, Color.Blue);
+        PhysicsObject siili = new PhysicsObject(100, 50);
+        siili.Shape = Shape.Circle;
+        siili.Color = Color.Brown;
+        siili.Position = new Vector(-450, -350); 
+        this.Add(siili);
 
 
 

2a630a58f2bafa551ce0a805a1aac1c007b403f1
Auvinen Jenny Kaarina <jenny.k.auvinen@student.jyu.fi>
2024-10-29 18:57:46 +0200

    testausta

--- Siili/Siili/Siili.cs
+++ Siili/Siili/Siili.cs
@@ -16,34 +16,110 @@ namespace Siili;
 /// </summary>
 
 /// TODO: - Suunnitellaan pelin rakenne -> luodaan pelin taso -> lisätään esteet ja keräilyesineet.
+/// https://tim.jyu.fi/view/kurssit/jypeli/kentat/ruutukentta#NTxBwYrXAJeI
 //  Luodaan pelihahmo ja määritellään perusliikkeet (kävely, hyppy).
 public class Siili : PhysicsGame
 {
+    private const double NOPEUS = 200;
+    private const double HYPPYNOPEUS = 750;
+    private const int RUUDUN_KOKO = 40;
+    private PlatformCharacter siili;
     public override void Begin()
     {
-       // TileMap ruudut = TileMap.FromLevelAsset("pelikentta");
+        Gravity = new Vector(0, -1000);
 
-        //ruudut.SetTileMethod("S", LuoSiili);
-        //ruudut.SetTileMethod("M", LuoMarja);
-        //ruudut.SetTileMethod("#", LuoPuu);
-        //ruudut.SetTileMethod("/", LuoSeina);
+        LuoKentta();
+        //LisaaNappaimet();
+
+        Camera.ZoomToLevel();   
+        Camera.Follow(siili);
+        Camera.ZoomFactor = 1.2;
+        Camera.StayInLevel = true;
+        
+        //MasterVolume = 0.5;
+
+    }
+
+    //Liikkuminen -> näppäimet
+    //   PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+    //   Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
 
 
-        //Siili peliin 
+    void LuoKentta()
+    {
+        TileMap kentta = TileMap.FromLevelAsset("pelikentta");
+
+        kentta.SetTileMethod('S', LuoSiili);
+        kentta.SetTileMethod('M', LuoMarja);
+        kentta.SetTileMethod('#', LuoPuu);
+
+        kentta.Execute(RUUDUN_KOKO, RUUDUN_KOKO);
+
         Level.Background.CreateGradient(Color.Green, Color.Blue);
-        PhysicsObject siili = new PhysicsObject(100, 50);
+        //Surface alareuna = Surface.CreateBottom(Level, 50, 300, 50, 30);
+        //alareuna.Color = Color.Brown;
+        //Add(alareuna);
+        Level.CreateBorders(false);
+
+        //Camera.ZoomToLevel();
+
+        /*PlatformCharacter siili = new PlatformCharacter(100, 50);
         siili.Shape = Shape.Circle;
         siili.Color = Color.Brown;
-        siili.Position = new Vector(-450, -350); 
+        siili.Position = new Vector(-450, -350);
         this.Add(siili);
+        
+       /* GameObject marja = new GameObject(40, 20);
+        marja.X = -200.0;
+        marja.Y = 0.0;
+        marja.Shape = Shape.Circle;
+        marja.Color = Color.Red;
+        this.Add(marja);
+
+        GameObject puu = new GameObject(50, 30);
+        puu.X = -190.0;
+        puu.Y = -20.0;
+        puu.Shape = Shape.Rectangle;
+        puu.Color = Color.Brown;
+        this.Add(puu);
+        */
 
 
-
-
-
-
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
-}
 
+    private void LuoPuu(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject puu = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        puu.Position = paikka;
+        puu.Color = Color.Green;
+        this.Add(puu);
+    }
+
+      private void LuoMarja(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject marja = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        marja.IgnoresCollisionResponse = true;
+        marja.Position = paikka;
+        marja.Shape = Shape.Circle;
+        marja.Color = Color.Red;
+        //tahti.Image = tahtiKuva;
+        //tahti.Tag = "tahti";
+        this.Add(marja);
+    }
+
+     private void LuoSiili(Vector paikka, double leveys, double korkeus)
+    {
+        siili = new PlatformCharacter(leveys, korkeus);
+        siili.Position = paikka;
+        siili.Mass = 4.0;
+        siili.Shape = Shape.Circle;
+        siili.Color = Color.Brown;
+        //pelaaja1.Image = pelaajanKuva;
+        //AddCollisionHandler(pelaaja1, "tahti", TormaaTahteen);
+        this.Add(siili);
+    }
+
+
+
+
+}
